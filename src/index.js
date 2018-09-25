ac831ea6e049402ada4a31b4de41fd231a89015c
Ivan Tsar <ivan.tsar99@gmail.com>
2018-09-25 23:41:02 +0300

    4th solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+
+	var eqDiv = equation.split(' '); //Divided - разделенный;
+	var solution = [];
+	
+	var a = eqDiv[0];
+	var b = eqDiv[3] + eqDiv[4];
+	var c = eqDiv[7] + eqDiv[8];
+
+	var D = Math.pow(b, 2) - 4 * a * c;	
+
+	solution.push(Math.round((-b + Math.pow(D, 0.5)) / (2 * a)));
+	solution.push(Math.round((-b - Math.pow(D, 0.5)) / (2 * a)));
+
+	if (solution[0] > solution[1]) {
+		var temp = solution [0];
+		solution[0] = solution[1];
+		solution[1] = temp;
+	}
+
+	return solution;
+
 }
+
